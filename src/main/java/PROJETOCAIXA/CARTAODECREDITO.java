e1d9e9a23e08c63f7a3645ca5a4facd8ba34c829
User <User@DESKTOP-D57QA5C>
2022-11-29 13:58:33 -0300

    //TESTE

---
+++ src/main/java/PROJETOCAIXA/CARTAODECREDITO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PROJETOCAIXA;
+
+
+public class CARTAODECREDITO {
+private String nome;
+private int idConta;
+private int senha;
+
+
+private int numeroCartaoCredito;
+private int CCV;
+private int dataVencimento;
+
+
+public  CARTAODECREDITO(String nome){
+this.nome = nome;
+this.idConta = idConta;
+this.senha = senha;
+
+this.numeroCartaoCredito = numeroCartaoCredito;
+this.CCV = CCV;
+this.dataVencimento = dataVencimento;
+}
+
+}
+
